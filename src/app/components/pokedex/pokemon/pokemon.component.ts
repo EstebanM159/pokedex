32df7897580637804eb9aed988e674cbccc34086
EstebanM159 <estebanm91231@gmail.com>
2023-07-18 20:16:46 -0300

    XD

--- src/app/components/pokedex/pokemon/pokemon.component.ts
+++ src/app/components/pokedex/pokemon/pokemon.component.ts
@@ -6,22 +6,15 @@ import { InfoTipos, Tipos } from 'src/app/models/pokemonApi';
   templateUrl: './pokemon.component.html',
   styleUrls: ['./pokemon.component.css']
 })
-export class PokemonComponent implements OnChanges{
+export class PokemonComponent{
   @Input() name: string="";
   @Input() img: string="";
   @Input()tipo : InfoTipos[]=[];
   public clase:string='';
-  ngOnChanges() {
-    this.clase = this.cambiarClase();
-    // console.log(this.tipo[0].type);
-    //Toma el primer tipo y la cambia la clase a los dos tipo :C
-  }
-  cambiarClase(){
-    let clase:string="";
-    this.tipo.forEach( e => {
-      console.log(e.type.name)
 
-      switch (e.type.name) {
+  color(tipo:string):string{
+    let clase:string="";
+    switch (tipo) {
       case 'fire':
         clase = 'bg-fuego';
         break;
@@ -49,8 +42,7 @@ export class PokemonComponent implements OnChanges{
       default:
         break;
     }
-    });
     return clase;
-
   }
+
 }
