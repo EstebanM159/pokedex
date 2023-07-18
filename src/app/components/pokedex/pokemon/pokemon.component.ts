d0fa701c51075b3f0594c0109a63db81e1b68fce
EstebanM159 <estebanm91231@gmail.com>
2023-07-18 17:24:08 -0300

    Estilos a los tipos

--- src/app/components/pokedex/pokemon/pokemon.component.ts
+++ src/app/components/pokedex/pokemon/pokemon.component.ts
@@ -1,16 +1,56 @@
-import { Component, Input, OnInit } from '@angular/core';
-
-
+import { Component, Input, OnChanges } from '@angular/core';
+import { InfoTipos, Tipos } from 'src/app/models/pokemonApi';
 
 @Component({
   selector: 'app-pokemon',
   templateUrl: './pokemon.component.html',
   styleUrls: ['./pokemon.component.css']
 })
-export class PokemonComponent {
-@Input() name: string="";
-@Input() img: string="";
-  @Input()type : any[]=[];
+export class PokemonComponent implements OnChanges{
+  @Input() name: string="";
+  @Input() img: string="";
+  @Input()tipo : InfoTipos[]=[];
+  public clase:string='';
+  ngOnChanges() {
+    this.clase = this.cambiarClase();
+    // console.log(this.tipo[0].type);
+    //Toma el primer tipo y la cambia la clase a los dos tipo :C
+  }
+  cambiarClase(){
+    let clase:string="";
+    this.tipo.forEach( e => {
+      console.log(e.type.name)
 
+      switch (e.type.name) {
+      case 'fire':
+        clase = 'bg-fuego';
+        break;
+      case 'grass':
+        clase = 'bg-grass';
+      break;
+      case 'flying':
+        clase = 'bg-flying';
+      break;
+      case 'electric':
+        clase = 'bg-electric';
+      break;
+      case 'poison':
+        clase = 'bg-poisson';
+      break;
+      case 'water':
+        clase = 'bg-water';
+      break;
+      case 'flighting':
+        clase = 'bg-flighting';
+      break;
+      case 'normal':
+        clase = 'bg-normal';
+      break;
+      default:
+        break;
+    }
+    });
+    return clase;
 
+  }
 }
--- src/app/models/pokemonApi.ts
+++ src/app/models/pokemonApi.ts
@@ -4,17 +4,17 @@ export interface Data{
   previos:string,
   results:Resultado[]
 }
-export interface Resultado{
-  name:string,
-  url:string
-}
 export interface Tipos{
   cant:InfoTipos
 
 }
 export interface InfoTipos{
-  slot:1,
+  slot:number,
   type:Resultado
+}
+export interface Resultado{
+  name:string,
+  url:string
 }
 // export interface PokemonApi{
 //   count:number,
