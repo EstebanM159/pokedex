96136dc9e9070f554eba2b9617302acefdcfcd5f
EstebanM159 <estebanm91231@gmail.com>
2023-07-27 19:15:33 -0300

    Posible fin

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -10,17 +10,17 @@ import { Pokemon } from 'src/app/models/pokemon';
 export class HeaderComponent {
 
   constructor (private consultaService : ConsultaApiService ){}
+  activo:boolean=false;
   pokemonBuscado?:Pokemon;
  errorMessage: string = "";
   public pokemonName:string="";
   buscar(e:any){
     this.pokemonName = e.target.value;
-
   }
   onClick(){
     this.consultaService.getPokemonByName(this.pokemonName).subscribe(data=>{
       this.pokemonBuscado=data
-      console.log(data);
+      this.activo=true;
     }
       ,
       (error=> {if(error===404)
